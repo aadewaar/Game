0355bc7860a99b7323ff0056b4464a45db1db59b
Alvin de Waard <aadewaar@asu.edu>
2019-02-17 21:34:58 -0700

    Screen

---
+++ Game/Assets/Project/Scripts/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public static class SaveSystem {
+
+    public static void SavePlayer (FirstPersonController FirstPersonController)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.savedata";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        Save save = new Save(FirstPersonController);
+
+        formatter.Serialize(stream, save);
+        stream.Close();
+    }
+
+    public static Save LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.savedata";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            Save save = formatter.Deserialize(stream) as Save;
+            stream.Close();
+
+            return save;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
